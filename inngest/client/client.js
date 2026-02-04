08a304bfcd2e02ba8f0bceaccee61c464907effd
ArhamX24 <arhamabdullah9045@gmail.com>
2026-02-04 18:45:21 +0530

    added fetch

--- inngest/client/client.js
+++ inngest/client/client.js
@@ -5,7 +5,8 @@ const inngest = new Inngest({
     id: "Admix-Ai-Studio",
     eventKey: "fhw5juKBxwnP0h4cxvWlM0hlPmva8UOnHAFG1QVPGdkuDiDeuWFKguZTWt3uisIg8420Ksq5xJSlAZq-4lOsAA",
     baseUrl: "https://inn.gs/",
-    isDev: false               
+    isDev: false,
+    fetch: fetch.bind(globalThis)               
 });
 
 export default inngest;
