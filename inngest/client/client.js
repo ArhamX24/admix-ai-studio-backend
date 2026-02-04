9a08f98cdb09921d2addd283c333a92e2860322e
ArhamX24 <arhamabdullah9045@gmail.com>
2026-02-04 18:33:18 +0530

    added event key

--- inngest/client/client.js
+++ inngest/client/client.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === "production" && !process.env.INNGEST_EVENT_KEY) {
 const inngest = new Inngest({
     id: "Admix-Ai-Studio",
     // Explicitly fallback to an empty string to prevent "undefined" errors
-    eventKey: process.env.INNGEST_EVENT_KEY || "" 
+    eventKey: "fhw5juKBxwnP0h4cxvWlM0hlPmva8UOnHAFG1QVPGdkuDiDeuWFKguZTWt3uisIg8420Ksq5xJSlAZq-4lOsAA"
 });
 
 export default inngest;
