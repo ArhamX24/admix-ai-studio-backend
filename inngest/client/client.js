812c91febd32978d3c040f91a7907e0de328f7ec
ArhamX24 <arhamabdullah9045@gmail.com>
2026-02-04 18:39:16 +0530

    isdev false made

--- inngest/client/client.js
+++ inngest/client/client.js
@@ -1,14 +1,11 @@
 import { Inngest } from "inngest";
 
-// Optional: Add a check to see if the key is actually there
-if (process.env.NODE_ENV === "production" && !process.env.INNGEST_EVENT_KEY) {
-    console.error("CRITICAL: INNGEST_EVENT_KEY is missing in production!");
-}
 
 const inngest = new Inngest({
     id: "Admix-Ai-Studio",
-    // Explicitly fallback to an empty string to prevent "undefined" errors
-    eventKey: "fhw5juKBxwnP0h4cxvWlM0hlPmva8UOnHAFG1QVPGdkuDiDeuWFKguZTWt3uisIg8420Ksq5xJSlAZq-4lOsAA"
+    eventKey: "fhw5juKBxwnP0h4cxvWlM0hlPmva8UOnHAFG1QVPGdkuDiDeuWFKguZTWt3uisIg8420Ksq5xJSlAZq-4lOsAA",
+    baseUrl: "https://inn.gs/",
+    isDev: false               
 });
 
 export default inngest;
