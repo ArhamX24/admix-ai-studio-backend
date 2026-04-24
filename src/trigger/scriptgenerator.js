667696149a39effbd2ea2e48b564eb83d1665d11
ArhamX24 <arhamabdullah9045@gmail.com>
2026-04-24 14:55:51 +0530

    added new key name

--- src/trigger/scriptgenerator.js
+++ src/trigger/scriptgenerator.js
@@ -3,7 +3,7 @@ import OpenAI from "openai";
 import prisma from "../../DB/prisma.client";
 
 const groq = new OpenAI({
-  apiKey: process.env.GROQ_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
   baseURL: "https://api.groq.com/openai/v1",
 });
 
